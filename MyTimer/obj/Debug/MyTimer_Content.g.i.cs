22cd3c47b7325c2368b9a720ee7eb3a78bb14d59
theanarch <boris.torbica@gmail.com>
2019-05-30 13:24:37 +0200

    Changed XAML structure

--- MyTimer/obj/Debug/MyTimer_Content.g.i.cs
+++ MyTimer/obj/Debug/MyTimer_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pomodoro_kfo_icon.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/trayicons/pomodoro_kfo_icon.ico")]
 
 
